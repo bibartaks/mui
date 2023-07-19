d1f05bfd546b7bb7fe05402162564409c5fb13a9
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2023-07-19 17:35:47 +0600

    Deleted the grid and add it in the main note app

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Grid, Paper } from "@mui/material"
+import { Container, Grid, Paper } from "@mui/material"
 
 const Notes = () => {
   const [notes, setNotes] = useState([])
@@ -11,26 +11,15 @@ const Notes = () => {
   }, [])
 
   return (
-    <>
+    <Container>
       <Grid container>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>2</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>3</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>4</Paper>
-        </Grid>
+        {notes.map(note => (
+          <Grid item key={note.id} xs={12} md={6} lg={4}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
       </Grid>
-
-      {notes.map(note => (
-        <p key={note.id}>{note.title}</p>
-      ))}
-    </>
+    </Container>
   )
 }
 
